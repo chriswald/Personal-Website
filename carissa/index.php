77b55e8bccbe17471ae097d89d5a57f6fdb12342
Chris Wald <wald.j.chris@gmail.com>
2025-03-13 09:32:20 -0500

    Add a spot for Carissa

---
+++ carissa/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Carissa Roper</title>
+		<link rel="stylesheet" href="style/landingstyle.css" type="text/css">
+	</head>
+
+	<body>
+		<table class="landingcontent">
+			<tr class="headerrow"><td>
+				<div class="topname">Carissa</div>
+			</td></tr>
+			<tr class="contentrow"><td>
+				<div class="content">
+					<a href="resume.html"><div class="menulink">R&eacute;sum&eacute;</div></a>
+				</div>
+			</td></tr>
+			<tr class="headerrow"><td>
+				<div class="bottomname">Roper</div>
+			</td></tr>
+		</table>
+	</body>
+</html>
